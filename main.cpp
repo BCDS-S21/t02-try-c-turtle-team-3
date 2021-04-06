dff056bec4944d36c0e9cba2ecbe1036d90f098c
Slatteryc <>
2021-04-06 20:31:31 +0000

    Added user input

--- main.cpp
+++ main.cpp
@@ -7,14 +7,24 @@
 namespace ct = cturtle;  //This makes it possible to use the CTurtle commands using ct::
 #include <iostream> //for input & output
 using namespace std; //this makes it possible to use cin and cout without std::
+#include <string>
 
 int main() {
 	ct::TurtleScreen scr;
+
+  string yname;
+
+  cout << "What is your name?" << endl; 
+  
+  cin >> yname;
+
+  cout << "Hello," << yname;
 
 	scr.bgcolor({ "white" });
 	ct::Turtle turtle(scr);
 
 	turtle.speed(ct::TS_FASTEST);
+  
   scr.tracer(0);
 
 	turtle.fillcolor({ "yellow" });
@@ -22,7 +32,7 @@ int main() {
 	
   // middle of flower
   turtle.penup();
-  turtle.goTo(0, 50);
+  turtle.goTo(-30, 50);
   turtle.pendown();
   for (float i = 0; i < 360; i++) {
         turtle.forward(1);
@@ -32,7 +42,7 @@ int main() {
  // loop for petals
   
   turtle.penup();
-  turtle.goTo(-20,100);
+  turtle.goTo(-50,100);
 
   for (float i = 0; i < 300; i++) { 
         
@@ -53,12 +63,12 @@ int main() {
 
   turtle.hideturtle();
 
-  turtle.goTo(120, 50);
+  turtle.goTo(100, 50);
 
   turtle.pendown();
   turtle.fillcolor({"black"});
   turtle.begin_fill();
-  turtle.write("Aren't you glad it's spring!");
+  turtle.write("Aren't you glad it's spring, " + yname + "!");
   turtle.end_fill();
 
   //for (float i = 0; i < 2; i++) {
